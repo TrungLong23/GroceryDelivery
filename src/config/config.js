a02bc2a8cbde8118319c6f6431c730e554e5a9b0
Long Ho <long.ho@ecotruck.vn>
2024-12-11 22:19:25 +0700

    add config MongoDbStore

--- src/config/config.js
+++ src/config/config.js
@@ -1,33 +1,35 @@
-import 'dotenv/config'
-import fastifySession from '@fastify/session'
-import ConnectMongoDBSession from 'connect-mongodb-session'
-import { Admin } from '../models/index.js'
+import 'dotenv/config';
+import fastifySession from '@fastify/session';
+import ConnectMongoDBSession from 'connect-mongodb-session';
+import { Admin } from '../models/index.js';
 
-const MongoDBStore = ConnectMongoDBSession(fastifySession)
+const MongoDBStore = ConnectMongoDBSession(fastifySession);
 
 export const sessionStore = new MongoDBStore({
-    uri:process.env.MONGO_URI,
-    collection:'session'
-})
-sessionStore.on("error",(error)=> {
-    console.log("Session store  error",error);
-})
+    uri: process.env.MONGO_URI,
+    collection: 'session',
+    ssl: true, // Kích hoạt kết nối SSL
+    sslValidate: true, // Kiểm tra chứng chỉ SSL
+});
 
-export const authenticate = async(email,password) => {
-    if(email && password) {
-        const user = await Admin.findOne({email});
-            if(!user) {
-                return null
-            }
-            if(user.password === password)   {
-                return Promise.resolve({email: email,password: password});
-            }else {
-                return null;
-            }
-        }
+sessionStore.on("error", (error) => {
+    console.log("Session store error", error);
+});
+
+export const authenticate = async (email, password) => {
+    if (email && password) {
+        const user = await Admin.findOne({ email });
+        if (!user) {
             return null;
-    };
+        }
+        if (user.password === password) {
+            return Promise.resolve({ email, password });
+        } else {
+            return null;
+        }
+    }
+    return null;
+};
 
 export const PORT = process.env.PORT || 3000;
-
-export  const COOKIE_PASSWORD = process.env.COOKIE_PASSWORD;
+export const COOKIE_PASSWORD = process.env.COOKIE_PASSWORD;
