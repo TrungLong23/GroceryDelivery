ae2a17f8a989c69198a53eac7c44c73061bc0f12
Long Ho <long.ho@ecotruck.vn>
2024-12-12 20:06:57 +0700

    add config cookie password

--- src/config/config.js
+++ src/config/config.js
@@ -44,4 +44,4 @@ export const authenticate = async (email, password) => {
 
 
 export const PORT = process.env.PORT || 3000;
-export const COOKIE_PASSWORD = process.env.COOKIE_PASSWORD;
+export const COOKIE_PASSWORD = process.env.COOKIE_PASSWORD || crypto.randomBytes(32).toString("hex");
